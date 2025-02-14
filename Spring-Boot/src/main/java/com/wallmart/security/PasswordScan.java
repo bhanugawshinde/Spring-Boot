4376bfe0348e80d4c787a4f04087ffe25176cca5
bhanugawshinde <61377191+bhanugawshinde@users.noreply.github.com>
2025-02-15 01:07:11 +0530

    component-scan

---
+++ Spring-Boot/src/main/java/com/wallmart/security/PasswordScan.java
@@ -0,0 +1,10 @@
+package com.wallmart.security;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class PasswordScan {
+	public PasswordScan() {
+		System.out.println("PasswordScan - Constructor ...");
+	}
+}
---
+++ Spring-Boot/src/main/java/in/ashokit/user/UserBean.java
@@ -0,0 +1,10 @@
+package in.ashokit.user;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class UserBean {
+	public UserBean() {
+		System.out.println("user bean - Constructor ...");
+	}
+}
