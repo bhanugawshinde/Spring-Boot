406d9e98845e3584bdff48f214df62888f6e74d7
bhanugawshinde <61377191+bhanugawshinde@users.noreply.github.com>
2025-02-15 00:49:23 +0530

    ioc-container created

--- Spring-Boot/src/main/java/in/ashokit/Application.java
+++ Spring-Boot/src/main/java/in/ashokit/Application.java
@@ -2,12 +2,18 @@ package in.ashokit;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(Application.class, args);
+		ConfigurableApplicationContext context = SpringApplication.run(Application.class, args);
+		System.out.println("beans created : "+context.getBeanDefinitionCount());
+		String[] beanDefinitionNames = context.getBeanDefinitionNames();
+		for (String beanName : beanDefinitionNames) {
+			System.out.println(beanName);
+		}
 	}
 
 }
