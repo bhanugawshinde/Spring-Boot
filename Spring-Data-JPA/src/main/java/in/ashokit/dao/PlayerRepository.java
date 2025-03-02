5d17bda114cd65f04ae871853c9751dd68be3d24
bhanugawshinde <61377191+bhanugawshinde@users.noreply.github.com>
2025-03-03 00:12:03 +0530

    findBy-method

--- Spring-Data-JPA/src/main/java/in/ashokit/dao/PlayerRepository.java
+++ Spring-Data-JPA/src/main/java/in/ashokit/dao/PlayerRepository.java
@@ -1,9 +1,40 @@
 package in.ashokit.dao;
 
+import java.util.List;
+
 import org.springframework.data.repository.CrudRepository;
 
 import in.ashokit.entity.Player;
 
 public interface PlayerRepository extends CrudRepository<Player, Integer> {
+	
+	//Player Name
+	public List<Player> findByPlayerName(String pname);
+	public List<Player> findByPlayerNameIn(List<String> names);
+	public List<Player> findByPlayerNameAndPlayerAge(String pname, String age);
+	public List<Player> findByPlayerNameAndLocation(String pname, String location);
+	public List<Player> findByPlayerNameOrPlayerAge(String pname, String age);
+	public List<Player> findByPlayerNameOrLocation(String pname, String location);
+	
+	//Player PlayerAge
+	public List<Player> findByPlayerAge(String age);
+	public List<Player> findByPlayerAgeIn(List<String> ages);
+	public List<Player> findByPlayerAgeBetween(String startPlayerAge, String endPlayerAge);
+	public List<Player> findByPlayerAgeBefore(String endPlayerAge);
+	public List<Player> findByPlayerAgeAfter(String startPlayerAge);
+	public List<Player> findByPlayerAgeOrLocation(String age, String location);
+	public List<Player> findByPlayerAgeAndLocation(String age, String location);
+	
+	//Location
+	public List<Player> findByLocation(String location);
+	public List<Player> findByLocationAllIgnoreCase(String location);
+	public List<Player> findByLocationAllIgnoringCase(String location);
+	public List<Player> findByLocationContaining(String locations);
+	public List<Player> findByLocationContains(String locations);
+	public List<Player> findByLocationEndingWith(String EndWordOflocation);
+	public List<Player> findByLocationEndsWith(String EndWordOflocation);
+	public List<Player> findByLocationContainsOrderByPlayerAge(String location);
 
+	
+	
 }
